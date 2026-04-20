f50461bd2236677564dcab6d50d32d835a2aefa3
Sturm <savagesturm@gmail.com>
2026-04-19 21:48:39 -0600

    Add project files.

---
+++ CarRental/Form1.cs
@@ -0,0 +1,14 @@
+//Sturm Savage
+//RCET2265
+//Spring Semester 2026
+//https://github.com/savastur/RollOfTheDiceList.git
+namespace CarRental
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
