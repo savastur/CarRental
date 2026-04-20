80476be9cb63c4765b3dd7827e1bbaa8945f4f33
Sturm <savagesturm@gmail.com>
2026-04-19 21:49:32 -0600

    header

--- CarRental/Form1.cs
+++ CarRental/Form1.cs
@@ -1,7 +1,7 @@
 //Sturm Savage
 //RCET2265
 //Spring Semester 2026
-//https://github.com/savastur/RollOfTheDiceList.git
+//https://github.com/savastur/CarRental.git
 namespace CarRental
 {
     public partial class Form1 : Form
